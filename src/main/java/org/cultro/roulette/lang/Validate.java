9dc5bf1aad88ea3e5d918b1b20a4f4b243c21abf
Helixan <helixan@proton.me>
2023-04-12 17:02:34 -0400

    Added a few null check validations

--- src/main/java/org/cultro/roulette/lang/Validate.java
+++ src/main/java/org/cultro/roulette/lang/Validate.java
@@ -1,4 +1,22 @@
 package org.cultro.roulette.lang;
 
 public class Validate {
+
+    public static <T> boolean notNull(T object, String message) {
+        if (object == null) {
+            if (message != null) {
+                throw new NullPointerException(message);
+            } else {
+                throw new NullPointerException();
+            }
+        }
+        return true;
+    }
+
+    public static <T> boolean notNull(T object) {
+        if (object == null) {
+            throw new NullPointerException();
+        }
+        return true;
+    }
 }
