d67082298a45d2d3e4f0cdecd96220b90bf5a629
Helixan <helixan@proton.me>
2023-04-17 00:38:07 -0400

    Added more methods to BooleanUtils

--- src/main/java/org/cultro/roulette/lang/BooleanUtils.java
+++ src/main/java/org/cultro/roulette/lang/BooleanUtils.java
@@ -2,7 +2,49 @@ package org.cultro.roulette.lang;
 
 public class BooleanUtils {
 
-    public static int compareTo(boolean first, boolean second) {
+    public static boolean and(final boolean... values) {
+        Validate.notNull(values);
+        for (final boolean value : values) {
+            if (!value) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean and(final Boolean... values) {
+        Validate.notNull(values);
+        Validate.containsNoNullElements(values);
+        for (final Boolean value : values) {
+            if (!value) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean or(final boolean... values) {
+        Validate.notNull(values);
+        for (final boolean value : values) {
+            if (value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean or(final Boolean... values) {
+        Validate.notNull(values);
+        Validate.containsNoNullElements(values);
+        for (final Boolean value : values) {
+            if (value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int compareTo(final boolean first, final boolean second) {
         if (first && !second) {
             return 1;
         } else if (!first && second) {
