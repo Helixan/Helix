c7e36bd805f561498975f4a79e04a0b0966bc9a2
Helixan <helixan@proton.me>
2023-05-01 06:02:37 -0400

    Changed "Location" class to be named "SimpleLocation"

--- src/main/java/org/cultro/roulette/region/SimpleLocation.java
+++ src/main/java/org/cultro/roulette/region/SimpleLocation.java
@@ -4,27 +4,27 @@ import org.cultro.roulette.lang.Validate;
 
 import java.util.Objects;
 
-public final class Location {
+public final class SimpleLocation {
 
     private double x;
     private double y;
     private double z;
 
-    public Location(double x, double y, double z) {
+    public SimpleLocation(double x, double y, double z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
-    public Location(double x, double y) {
+    public SimpleLocation(double x, double y) {
         this(x, y, 0);
     }
 
-    public Location(double x) {
+    public SimpleLocation(double x) {
         this(x, 0, 0);
     }
 
-    public Location() {
+    public SimpleLocation() {
         this(0, 0, 0);
     }
 
@@ -52,7 +52,7 @@ public final class Location {
         this.z = z;
     }
 
-    public Location add(Location locationToAdd) {
+    public SimpleLocation add(SimpleLocation locationToAdd) {
         Validate.notNull(locationToAdd);
         this.x += locationToAdd.x;
         this.y += locationToAdd.y;
@@ -60,14 +60,14 @@ public final class Location {
         return this;
     }
 
-    public Location add(double x, double y, double z) {
+    public SimpleLocation add(double x, double y, double z) {
         this.x += x;
         this.y += y;
         this.z += z;
         return this;
     }
 
-    public Location subtract(Location locationToAdd) {
+    public SimpleLocation subtract(SimpleLocation locationToAdd) {
         Validate.notNull(locationToAdd);
         this.x -= locationToAdd.x;
         this.y -= locationToAdd.y;
@@ -75,14 +75,14 @@ public final class Location {
         return this;
     }
 
-    public Location subtract(double x, double y, double z) {
+    public SimpleLocation subtract(double x, double y, double z) {
         this.x -= x;
         this.y -= y;
         this.z -= z;
         return this;
     }
 
-    public Location multiply(Location locationToAdd) {
+    public SimpleLocation multiply(SimpleLocation locationToAdd) {
         Validate.notNull(locationToAdd);
         this.x *= locationToAdd.x;
         this.y *= locationToAdd.y;
@@ -90,14 +90,14 @@ public final class Location {
         return this;
     }
 
-    public Location multiply(double x, double y, double z) {
+    public SimpleLocation multiply(double x, double y, double z) {
         this.x *= x;
         this.y *= y;
         this.z *= z;
         return this;
     }
 
-    public Location divide(Location locationToAdd) {
+    public SimpleLocation divide(SimpleLocation locationToAdd) {
         Validate.notNull(locationToAdd);
         this.x /= locationToAdd.x;
         this.y /= locationToAdd.y;
@@ -105,14 +105,14 @@ public final class Location {
         return this;
     }
 
-    public Location divide(double x, double y, double z) {
+    public SimpleLocation divide(double x, double y, double z) {
         this.x /= x;
         this.y /= y;
         this.z /= z;
         return this;
     }
 
-    public Location zero() {
+    public SimpleLocation zero() {
         this.x = 0;
         this.y = 0;
         this.z = 0;
@@ -124,10 +124,10 @@ public final class Location {
         if (this == o) {
             return true;
         }
-        if (!(o instanceof Location)) {
+        if (!(o instanceof SimpleLocation)) {
             return false;
         }
-        Location location = (Location) o;
+        SimpleLocation location = (SimpleLocation) o;
         return Double.compare(location.x, x) == 0 && Double.compare(location.y, y) == 0 && Double.compare(location.z, z) == 0;
     }
 
