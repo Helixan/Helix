146f6a4c9c59c0a642db704fb7101ef9dbc09691
Helixan <helixan@proton.me>
2023-05-25 17:00:45 -0400

    Added a class that allows the sorting of maps either via key or by value

---
+++ src/main/java/org/cultro/roulette/util/MapSorter.java
@@ -0,0 +1,56 @@
+package org.cultro.roulette.util;
+
+import org.cultro.roulette.lang.Validate;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class MapSorter {
+
+    public static <K extends Comparable<K>, V> LinkedHashMap<K, V> sortByKey(Map<K, V> map, boolean reverseOrder) {
+        Validate.notNull(map, "A null map cannot be sorted");
+        return map.entrySet().stream()
+                .sorted(((o1, o2) -> reverseOrder ? o2.getKey().compareTo(o1.getKey()) : o1.getKey().compareTo(o2.getKey())))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+    }
+
+    public static <K extends Comparable<K>, V> LinkedHashMap<K, V> sortByKey(Map<K, V> map) {
+        return sortByKey(map, false);
+    }
+
+    public static <K, V extends Comparable<V>> LinkedHashMap<K, V> sortByValue(Map<K, V> map, boolean reverseOrder) {
+        Validate.notNull(map, "A null map cannot be sorted");
+        return map.entrySet().stream()
+                .sorted(((o1, o2) -> reverseOrder ? o2.getValue().compareTo(o1.getValue()) : o1.getValue().compareTo(o2.getValue())))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+    }
+
+    public static <K, V extends Comparable<V>> LinkedHashMap<K, V> sortByValue(Map<K, V> map) {
+        return sortByValue(map, false);
+    }
+
+    public static <K> K getKeyAtIndex(LinkedHashMap<K, ?> map, int index) {
+        Validate.notNull(map, "A null map does not have keys");
+        Validate.isValidIndex(index, "An index may not be negative");
+        for (K current : map.keySet()) {
+            if (index == 0) {
+                return current;
+            }
+            index--;
+        }
+        throw new IndexOutOfBoundsException();
+    }
+
+    public static <V> V getValueAtIndex(LinkedHashMap<?, V> map, int index) {
+        Validate.notNull(map, "A null map does not have keys");
+        Validate.isValidIndex(index, "An index may not be negative");
+        for (V current : map.values()) {
+            if (index == 0) {
+                return current;
+            }
+            index--;
+        }
+        throw new IndexOutOfBoundsException();
+    }
+}
