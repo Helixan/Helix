827ee1858079af71dfcd787b4f4637b6ef6f4f48
Helixan <helixan@proton.me>
2023-06-06 07:35:49 -0400

    Added Comments to ArrayUtils

--- src/main/java/org/cultro/roulette/util/ArrayUtils.java
+++ src/main/java/org/cultro/roulette/util/ArrayUtils.java
@@ -1816,6 +1816,14 @@ public final class ArrayUtils {
         reverse(array, 0, array.length - 1);
     }
 
+
+    /**
+     * Reverses the elements in a float array within the specified bounds
+     *
+     * @param array the float array to be reversed
+     * @param firstBoundInclusive the index of the first element to be reversed (inclusive)
+     * @param secondBoundInclusive the index of the last element to be reversed (inclusive)
+     */
     public static void reverse(final int[] array, final int firstBoundInclusive, final int secondBoundInclusive) {
         if (array == null) {
             return;
