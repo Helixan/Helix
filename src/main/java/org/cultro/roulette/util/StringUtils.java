8613116132f01a5791edf607c8a64890bdf37f82
Helixan <helixan@proton.me>
2023-06-12 18:29:05 -0400

    Added a method to check if a string is a long

--- src/main/java/org/cultro/roulette/util/StringUtils.java
+++ src/main/java/org/cultro/roulette/util/StringUtils.java
@@ -102,4 +102,13 @@ public final class StringUtils {
             return false;
         }
     }
+
+    public static boolean isLong(String value) {
+        try {
+            Long.parseLong(value);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
