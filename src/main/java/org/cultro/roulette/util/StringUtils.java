8cbd99f5da7510accf1d0fb23885b5c096fe0166
Helixan <helixan@proton.me>
2023-06-12 17:31:53 -0400

    Finalized some variables

--- src/main/java/org/cultro/roulette/util/StringUtils.java
+++ src/main/java/org/cultro/roulette/util/StringUtils.java
@@ -6,10 +6,10 @@ import org.cultro.roulette.lang.Validate;
 @SuppressWarnings("unused")
 public final class StringUtils {
 
-    public static int editDistance(String first, String second) {
+    public static int editDistance(final String first, final String second) {
         Validate.notNull(first, "No edit distance can be calculated on a null String");
         Validate.notNull(second, "No edit distance can be calculated on a null String");
-        int[][] table = new int[first.length() + 1][second.length() + 1];
+        final int[][] table = new int[first.length() + 1][second.length() + 1];
 
         for (int i = 0; i <= first.length(); i++) {
             for (int j = 0; j <= second.length(); j++) {
@@ -25,43 +25,43 @@ public final class StringUtils {
         return table[first.length()][second.length()];
     }
 
-    public static int editDistance(String first, RString second) {
+    public static int editDistance(final String first, final RString second) {
         Validate.notNull(first, "No edit distance can be calculated on a null String");
         Validate.notNull(second, "No edit distance can be calculated on a null RString");
         return editDistance(first, second.toString());
     }
 
-    public static int editDistance(RString first, String second) {
+    public static int editDistance(final RString first, final String second) {
         Validate.notNull(first, "No edit distance can be calculated on a null RString");
         Validate.notNull(second, "No edit distance can be calculated on a null String");
         return editDistance(first.toString(), second);
     }
 
-    public static int editDistance(RString first, RString second) {
+    public static int editDistance(final RString first, final RString second) {
         Validate.notNull(first, "No edit distance can be calculated on a null RString");
         Validate.notNull(second, "No edit distance can be calculated on a null RString");
         return editDistance(first.toString(), second.toString());
     }
 
-    public static int editDistanceIgnoreCase(String first, String second) {
+    public static int editDistanceIgnoreCase(final String first, final String second) {
         Validate.notNull(first, "No edit distance can be calculated on a null String");
         Validate.notNull(second, "No edit distance can be calculated on a null String");
         return editDistance(first.toLowerCase(), second.toLowerCase());
     }
 
-    public static int editDistanceIgnoreCase(String first, RString second) {
+    public static int editDistanceIgnoreCase(final String first, final RString second) {
         Validate.notNull(first, "No edit distance can be calculated on a null String");
         Validate.notNull(second, "No edit distance can be calculated on a null RString");
         return editDistance(first.toLowerCase(), second.toString().toLowerCase());
     }
 
-    public static int editDistanceIgnoreCase(RString first, String second) {
+    public static int editDistanceIgnoreCase(final RString first, final String second) {
         Validate.notNull(first, "No edit distance can be calculated on a null RString");
         Validate.notNull(second, "No edit distance can be calculated on a null String");
         return editDistance(first.toString().toLowerCase(), second.toLowerCase());
     }
 
-    public static int editDistanceIgnoreCase(RString first, RString second) {
+    public static int editDistanceIgnoreCase(final RString first, final RString second) {
         Validate.notNull(first, "No edit distance can be calculated on a null RString");
         Validate.notNull(second, "No edit distance can be calculated on a null RString");
         return editDistance(first.toString().toLowerCase(), second.toString().toLowerCase());
