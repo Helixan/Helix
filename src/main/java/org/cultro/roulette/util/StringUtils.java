397cde50d8b4505915dc4328bedf832dce6e1684
Helixan <helixan@proton.me>
2023-06-12 16:58:00 -0400

    Added a method to calculate the edit distance between two strings

--- src/main/java/org/cultro/roulette/util/StringUtils.java
+++ src/main/java/org/cultro/roulette/util/StringUtils.java
@@ -2,4 +2,21 @@ package org.cultro.roulette.util;
 
 @SuppressWarnings("unused")
 public final class StringUtils {
+
+    public static int editDistance(String first, String second) {
+        int[][] table = new int[first.length() + 1][second.length() + 1];
+
+        for (int i = 0; i <= first.length(); i++) {
+            for (int j = 0; j <= second.length(); j++) {
+                if (i == 0) {
+                    table[i][j] = j;
+                } else if (j == 0) {
+                    table[i][j] = i;
+                } else {
+                    table[i][j] = MathUtils.min(table[i - 1][j - 1] + (first.charAt(i - 1) == second.charAt(j - 1) ? 0 : 1), table[i - 1][j] + 1, table[i][j - 1] + 1);
+                }
+            }
+        }
+        return table[first.length()][second.length()];
+    }
 }
