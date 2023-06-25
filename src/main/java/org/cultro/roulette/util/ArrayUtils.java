cd3ef41f830233c7ebc7c4c55d4954e330dc5208
Helixan <helixan@proton.me>
2023-06-25 01:31:10 -0400

    Added comments to ArrayUtils

--- src/main/java/org/cultro/roulette/util/ArrayUtils.java
+++ src/main/java/org/cultro/roulette/util/ArrayUtils.java
@@ -264,6 +264,16 @@ public final class ArrayUtils {
         return newArray;
     }
 
+
+    /**
+     * Concatenates the input array with the varargs array by creating a new array that includes all elements
+     * of the initial array followed by all elements of the varargs array
+     *
+     * @param array the array to which the elements are to be added
+     * @param elementsToAdd the elements to be added to the array
+     * @return a new array that contains all elements of the input array followed by all elements of the
+     * input varargs array
+     */
     public static float[] addAll(final float[] array, final float... elementsToAdd) {
         if (elementsToAdd == null) {
             return clone(array);
