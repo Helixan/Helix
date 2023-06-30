03077f4e8cd922df6eb765038d70766be90b155c
Helixan <helixan@proton.me>
2023-06-30 18:00:18 -0400

    Added comments to ArrayUtils

--- src/main/java/org/cultro/roulette/util/ArrayUtils.java
+++ src/main/java/org/cultro/roulette/util/ArrayUtils.java
@@ -1599,7 +1599,7 @@ public final class ArrayUtils {
 
 
     /**
-     * Reverses the elements in a boolean array within the specified bounds
+     * Reverses the elements in an array within the specified bounds
      *
      * @param array the array to be reversed
      * @param firstBoundInclusive the index of the first element to be reversed (inclusive)
