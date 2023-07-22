0fe835dff1520349cf602d8220923b8ff087166d
Helixan <helixan@proton.me>
2023-07-22 19:00:35 -0400

    Added methods to see a comparable object is less than another

--- src/main/java/org/cultro/roulette/lang/Validate.java
+++ src/main/java/org/cultro/roulette/lang/Validate.java
@@ -226,6 +226,42 @@ public final class Validate {
         throw new IllegalArgumentException();
     }
 
+    public static <T extends Comparable<T>> void isLessThan(T first, T second, String message) {
+        if (first != null && second != null) {
+            if (first.compareTo(second) < 0) {
+                return;
+            }
+        }
+        throw new IllegalArgumentException(message);
+    }
+
+    public static <T extends Comparable<T>> void isLessThan(T first, T second) {
+        if (first != null && second != null) {
+            if (first.compareTo(second) < 0) {
+                return;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public static <T extends Comparable<T>> void isLessThanOrEqualTo(T first, T second, String message) {
+        if (first != null && second != null) {
+            if (first.compareTo(second) <= 0) {
+                return;
+            }
+        }
+        throw new IllegalArgumentException(message);
+    }
+
+    public static <T extends Comparable<T>> void isLessThanOrEqualTo(T first, T second) {
+        if (first != null && second != null) {
+            if (first.compareTo(second) <= 0) {
+                return;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
     public static void isValidIndex(int index, String message) {
         if (index < 0) {
             if (message != null) {
