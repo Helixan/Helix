5f062a527a7bf42316935b15ad682da8bc209d60
Helixan <helixan@proton.me>
2023-07-23 14:16:32 -0400

    Put the methods in alphabetic order

--- src/main/java/org/cultro/roulette/util/MapUtils.java
+++ src/main/java/org/cultro/roulette/util/MapUtils.java
@@ -8,28 +8,6 @@ import java.util.stream.Collectors;
 @SuppressWarnings("unused")
 public final class MapUtils {
 
-    public static <K extends Comparable<K>, V> LinkedHashMap<K, V> sortByKey(Map<K, V> map, boolean reverseOrder) {
-        Validate.notNull(map, "A null map cannot be sorted");
-        return map.entrySet().stream()
-                .sorted(((o1, o2) -> reverseOrder ? o2.getKey().compareTo(o1.getKey()) : o1.getKey().compareTo(o2.getKey())))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
-    }
-
-    public static <K extends Comparable<K>, V> LinkedHashMap<K, V> sortByKey(Map<K, V> map) {
-        return sortByKey(map, false);
-    }
-
-    public static <K, V extends Comparable<V>> LinkedHashMap<K, V> sortByValue(Map<K, V> map, boolean reverseOrder) {
-        Validate.notNull(map, "A null map cannot be sorted");
-        return map.entrySet().stream()
-                .sorted(((o1, o2) -> reverseOrder ? o2.getValue().compareTo(o1.getValue()) : o1.getValue().compareTo(o2.getValue())))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
-    }
-
-    public static <K, V extends Comparable<V>> LinkedHashMap<K, V> sortByValue(Map<K, V> map) {
-        return sortByValue(map, false);
-    }
-
     public static <K> K getKeyAtIndex(LinkedHashMap<K, ?> map, int index) {
         Validate.notNull(map, "A null map does not have keys");
         Validate.isValidIndex(index, "An index may not be negative");
@@ -60,5 +38,27 @@ public final class MapUtils {
             index--;
         }
         throw new IndexOutOfBoundsException();
+    }
+
+    public static <K extends Comparable<K>, V> LinkedHashMap<K, V> sortByKey(Map<K, V> map, boolean reverseOrder) {
+        Validate.notNull(map, "A null map cannot be sorted");
+        return map.entrySet().stream()
+                .sorted(((o1, o2) -> reverseOrder ? o2.getKey().compareTo(o1.getKey()) : o1.getKey().compareTo(o2.getKey())))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+    }
+
+    public static <K extends Comparable<K>, V> LinkedHashMap<K, V> sortByKey(Map<K, V> map) {
+        return sortByKey(map, false);
+    }
+
+    public static <K, V extends Comparable<V>> LinkedHashMap<K, V> sortByValue(Map<K, V> map, boolean reverseOrder) {
+        Validate.notNull(map, "A null map cannot be sorted");
+        return map.entrySet().stream()
+                .sorted(((o1, o2) -> reverseOrder ? o2.getValue().compareTo(o1.getValue()) : o1.getValue().compareTo(o2.getValue())))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+    }
+
+    public static <K, V extends Comparable<V>> LinkedHashMap<K, V> sortByValue(Map<K, V> map) {
+        return sortByValue(map, false);
     }
 }
