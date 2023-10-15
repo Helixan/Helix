d09edb1fc732e7def32ee8652460d1c01368ad7a
Helixan <helixan@proton.me>
2023-10-15 05:04:22 -0400

    Added Keccack

---
+++ src/main/java/org/cultro/roulette/security/hashing/keccak/Keccack512Hasher.java
@@ -0,0 +1,42 @@
+package org.cultro.roulette.security.hashing.keccak;
+
+import org.bouncycastle.crypto.digests.KeccakDigest;
+import org.cultro.roulette.security.hashing.Hash;
+import org.cultro.roulette.security.hashing.HashAlgorithm;
+
+public class Keccack512Hasher implements Hash {
+
+    private final KeccakDigest keccakDigest;
+
+    public Keccack512Hasher() {
+        this.keccakDigest = new KeccakDigest(512);
+    }
+
+    @Override
+    public byte[] digest(byte[] input) {
+        keccakDigest.update(input, 0, input.length);
+        byte[] out = new byte[keccakDigest.getDigestSize()];
+        keccakDigest.doFinal(out, 0);
+        return out;
+    }
+
+    @Override
+    public byte[] digest(byte[] input, byte[] salt) {
+        byte[] combinedInput = new byte[input.length + salt.length];
+        System.arraycopy(input, 0, combinedInput, 0, input.length);
+        System.arraycopy(salt, 0, combinedInput, input.length, salt.length);
+
+        return digest(combinedInput);
+    }
+
+    @Override
+    public HashAlgorithm getHashAlgorithm() {
+        return HashAlgorithm.KECCACK_512;
+    }
+
+    @Override
+    public int getHashLength() {
+        return 64;
+    }
+}
+
