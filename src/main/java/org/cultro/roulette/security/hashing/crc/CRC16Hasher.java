8b8f639af39afada80d2b1756cfcfa6164c6b27b
Helixan <helixan@proton.me>
2023-12-17 04:59:32 -0500

    Added documentation

--- src/main/java/org/cultro/roulette/security/hashing/crc/CRC16Hasher.java
+++ src/main/java/org/cultro/roulette/security/hashing/crc/CRC16Hasher.java
@@ -5,27 +5,31 @@ import org.cultro.roulette.security.hashing.HashAlgorithm;
 
 /**
  * Implementation of the Hash interface for the CRC16 algorithm.
+ * CRC16 (Cyclic Redundancy Check) is a common checksum algorithm used to detect errors in data.
  */
 public class CRC16Hasher implements Hash {
 
     /**
      * Computes the CRC16 hash digest of the given input.
+     * This method applies the CRC16 algorithm to calculate the checksum from the input byte array.
      *
      * @param input The byte array to be hashed.
-     * @return The hashed byte array.
+     * @return The hashed byte array, representing the CRC16 checksum.
      */
     @Override
     public byte[] digest(byte[] input) {
         return computeCRC16(input);
     }
 
+
     /**
      * Computes the CRC16 hash digest of the given input with optional prefix and suffix salts.
      * The resulting hash is computed over the concatenated value of prefixSalt, input, and suffixSalt.
+     * This allows for additional data manipulation before the CRC16 computation.
      *
-     * @param input       The byte array to be hashed.
-     * @param salt        The byte array to be appended to the input before hashing.
-     * @return The hashed byte array.
+     * @param input The byte array to be hashed.
+     * @param salt  The byte array to be appended to the input before hashing.
+     * @return The hashed byte array, representing the CRC16 checksum.
      */
     @Override
     public byte[] digest(byte[] input, byte[] salt) {
@@ -36,28 +40,33 @@ public class CRC16Hasher implements Hash {
         return computeCRC16(combinedInput);
     }
 
+
     /**
-     * Retrieves the specific hashing algorithm used by the implementation.
+     * Retrieves the specific hashing algorithm used by the implementation, CRC16.
      *
-     * @return The hashing algorithm.
+     * @return The hashing algorithm, CRC16.
      */
     @Override
     public HashAlgorithm getHashAlgorithm() {
         return HashAlgorithm.CRC16;
     }
 
+
     /**
      * Retrieves the length of the hash produced by the implementation.
+     * The CRC16 algorithm produces a 2-byte checksum.
      *
-     * @return The length of the hash in bytes.
+     * @return The length of the hash in bytes (2 bytes).
      */
     @Override
     public int getHashLength() {
         return 2;
     }
 
+
     /**
      * Computes the CRC16 hash of the given byte array.
+     * This private method encapsulates the CRC16 algorithm logic.
      *
      * @param data The byte array to be hashed.
      * @return The CRC16 hash as a byte array.
