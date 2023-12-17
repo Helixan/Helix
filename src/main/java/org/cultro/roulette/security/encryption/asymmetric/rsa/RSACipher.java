2172c1b9961677a8b61e283bd0efff509284ca1c
Helixan <helixan@proton.me>
2023-12-17 05:02:13 -0500

    Added documentation

--- src/main/java/org/cultro/roulette/security/encryption/asymmetric/rsa/RSACipher.java
+++ src/main/java/org/cultro/roulette/security/encryption/asymmetric/rsa/RSACipher.java
@@ -1,6 +1,5 @@
 package org.cultro.roulette.security.encryption.asymmetric.rsa;
 
-
 import org.cultro.roulette.lang.Validate;
 import org.cultro.roulette.security.encryption.EncryptionAlgorithm;
 import org.cultro.roulette.security.encryption.asymmetric.AsymmetricCipher;
@@ -8,22 +7,43 @@ import org.cultro.roulette.security.encryption.asymmetric.AsymmetricCipher;
 import javax.crypto.Cipher;
 import java.security.*;
 
+/**
+ * Implements the RSACipher class, providing RSA encryption and decryption capabilities.
+ * This class supports operations such as key pair generation, encryption, and decryption using RSA algorithm.
+ */
 public class RSACipher implements AsymmetricCipher {
 
     private static final String RSA_ALGORITHM = "RSA";
     private static final String RSA_TRANSFORMATION = "RSA/ECB/PKCS1Padding";
-
     private final int keySize;
 
+
+    /**
+     * Constructor for RSACipher with a specified key size.
+     *
+     * @param keySize The size of the RSA key in bits. Must be greater than or equal to 512.
+     */
     public RSACipher(int keySize) {
         Validate.isGreaterThanOrEqualTo(keySize, 512, "Cannot have a RSA key size of less than 512.");
         this.keySize = keySize;
     }
 
+
+    /**
+     * Default constructor for RSACipher, which sets the key size to 2048 bits.
+     */
     public RSACipher() {
         this.keySize = 2048;
     }
 
+
+    /**
+     * Encrypts plaintext using the specified public RSA key.
+     *
+     * @param plaintext The plaintext to be encrypted.
+     * @param publicKey The RSA public key.
+     * @return The encrypted data.
+     */
     @Override
     public byte[] encrypt(byte[] plaintext, PublicKey publicKey) {
         try {
@@ -35,6 +55,14 @@ public class RSACipher implements AsymmetricCipher {
         }
     }
 
+
+    /**
+     * Decrypts ciphertext using the specified private RSA key.
+     *
+     * @param ciphertext The ciphertext to be decrypted.
+     * @param privateKey The RSA private key.
+     * @return The decrypted data.
+     */
     @Override
     public byte[] decrypt(byte[] ciphertext, PrivateKey privateKey) {
         try {
@@ -46,6 +74,12 @@ public class RSACipher implements AsymmetricCipher {
         }
     }
 
+
+    /**
+     * Generates a RSA key pair with the specified key size.
+     *
+     * @return A KeyPair containing RSA public and private keys.
+     */
     @Override
     public KeyPair generateKeyPair() {
         try {
@@ -57,6 +91,13 @@ public class RSACipher implements AsymmetricCipher {
         }
     }
 
+
+    /**
+     * Generates a RSA key pair using a specified seed for the random number generator.
+     *
+     * @param seed The seed to use for key pair generation.
+     * @return A KeyPair containing RSA public and private keys.
+     */
     @Override
     public KeyPair generateKeyPair(byte[] seed) {
         try {
@@ -70,6 +111,12 @@ public class RSACipher implements AsymmetricCipher {
         }
     }
 
+
+    /**
+     * Returns the encryption algorithm used, which is RSA in this implementation.
+     *
+     * @return The encryption algorithm, RSA.
+     */
     @Override
     public EncryptionAlgorithm getEncryptionAlgorithm() {
         return EncryptionAlgorithm.RSA;
