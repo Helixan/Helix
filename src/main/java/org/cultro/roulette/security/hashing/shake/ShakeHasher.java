93acb6fce205a4427dabe84e22692bc0e51d23f1
Helixan <helixan@proton.me>
2023-12-17 04:57:17 -0500

    Added documentation

--- src/main/java/org/cultro/roulette/security/hashing/shake/ShakeHasher.java
+++ src/main/java/org/cultro/roulette/security/hashing/shake/ShakeHasher.java
@@ -4,11 +4,23 @@ import org.cultro.roulette.security.hashing.Hash;
 import org.cultro.roulette.security.hashing.HashAlgorithm;
 import org.bouncycastle.crypto.digests.SHAKEDigest;
 
+/**
+ * This class implements the Hash interface using the SHAKE (Secure Hash Algorithm KECCAK)
+ * cryptographic hash functions, SHAKE128 and SHAKE256.
+ * It provides the functionality to compute hash digests with variable output lengths.
+ */
 public class ShakeHasher implements Hash {
 
     private final int length;
     private final SHAKEDigest shakeDigest;
 
+
+    /**
+     * Constructs a new ShakeHasher with the specified output hash length.
+     * Initializes the underlying SHAKE digest algorithm based on the length.
+     *
+     * @param length The desired length of the hash output in bytes.
+     */
     public ShakeHasher(int length) {
         this.length = length;
         if (length <= 16) {
@@ -18,6 +30,13 @@ public class ShakeHasher implements Hash {
         }
     }
 
+
+    /**
+     * Computes the hash digest of the given input using the SHAKE algorithm.
+     *
+     * @param input The byte array to be hashed.
+     * @return The hashed byte array with the specified output length.
+     */
     @Override
     public byte[] digest(byte[] input) {
         shakeDigest.update(input, 0, input.length);
@@ -26,6 +45,15 @@ public class ShakeHasher implements Hash {
         return out;
     }
 
+
+    /**
+     * Computes the hash digest of the given input, concatenating with the provided salt, using the SHAKE algorithm.
+     * This method combines the input array and salt array before computing the hash.
+     *
+     * @param input The byte array to be hashed.
+     * @param salt  The salt to be appended to the input before hashing.
+     * @return The hashed byte array with the specified output length.
+     */
     @Override
     public byte[] digest(byte[] input, byte[] salt) {
         byte[] combinedInput = new byte[input.length + salt.length];
@@ -35,11 +63,24 @@ public class ShakeHasher implements Hash {
         return digest(combinedInput);
     }
 
+
+    /**
+     * Retrieves the specific hashing algorithm used by this SHAKE hasher.
+     *
+     * @return The hashing algorithm, either SHAKE128 or SHAKE256.
+     */
     @Override
     public HashAlgorithm getHashAlgorithm() {
         return (shakeDigest.getDigestSize() == 16) ? HashAlgorithm.SHAKE128 : HashAlgorithm.SHAKE256;
     }
 
+
+    /**
+     *
+     * Retrieves the length of the hash produced by this SHAKE hasher.
+     *
+     * @return The length of the hash in bytes.
+     */
     @Override
     public int getHashLength() {
         return length;
