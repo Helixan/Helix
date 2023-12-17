cb9b0dc494cb8af42e5b79f0597aaf6ea5177936
Helixan <helixan@proton.me>
2023-12-17 04:57:41 -0500

    Added documentation

--- src/main/java/org/cultro/roulette/security/hashing/ripemd/RIPEMD160Hasher.java
+++ src/main/java/org/cultro/roulette/security/hashing/ripemd/RIPEMD160Hasher.java
@@ -4,14 +4,29 @@ import org.cultro.roulette.security.hashing.Hash;
 import org.cultro.roulette.security.hashing.HashAlgorithm;
 import org.bouncycastle.crypto.digests.RIPEMD160Digest;
 
+/**
+ * This class implements the Hash interface using the RIPEMD-160 (RACE Integrity Primitives Evaluation Message Digest) hashing algorithm.
+ * It provides methods for computing hash digests using RIPEMD-160.
+ */
 public class RIPEMD160Hasher implements Hash {
 
     private final RIPEMD160Digest ripemd160Digest;
 
+
+    /**
+     * Constructs a new RIPEMD160Hasher by initializing the RIPEMD-160 hashing algorithm.
+     */
     public RIPEMD160Hasher() {
         this.ripemd160Digest = new RIPEMD160Digest();
     }
 
+
+    /**
+     * Computes the hash digest of the given input using the RIPEMD-160 algorithm.
+     *
+     * @param input The byte array to be hashed.
+     * @return The hashed byte array (20 bytes).
+     */
     @Override
     public byte[] digest(byte[] input) {
         ripemd160Digest.update(input, 0, input.length);
@@ -20,6 +35,15 @@ public class RIPEMD160Hasher implements Hash {
         return out;
     }
 
+
+    /**
+     * Computes the hash digest of the given input, concatenating with the provided salt, using the RIPEMD-160 algorithm.
+     * This method combines the input array and salt array before computing the hash.
+     *
+     * @param input The byte array to be hashed.
+     * @param salt  The salt to be appended to the input before hashing.
+     * @return The hashed byte array (20 bytes).
+     */
     @Override
     public byte[] digest(byte[] input, byte[] salt) {
         byte[] combinedInput = new byte[input.length + salt.length];
@@ -29,11 +53,23 @@ public class RIPEMD160Hasher implements Hash {
         return digest(combinedInput);
     }
 
+
+    /**
+     * Retrieves the specific hashing algorithm used by this RIPEMD160 hasher.
+     *
+     * @return The hashing algorithm, RIPEMD-160.
+     */
     @Override
     public HashAlgorithm getHashAlgorithm() {
         return HashAlgorithm.RIPEMD160;
     }
 
+
+    /**
+     * Retrieves the length of the hash produced by this RIPEMD160 hasher.
+     *
+     * @return The length of the hash in bytes (20 bytes).
+     */
     @Override
     public int getHashLength() {
         return 20;
